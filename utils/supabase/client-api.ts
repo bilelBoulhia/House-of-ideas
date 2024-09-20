84ab3e3149c2c3f1de157b1e16ff0f40e45f46fa
bilel0509 <billel.boulahia456@gmail.com>
2024-09-20 09:10:35 +0100

    fixed a problem

--- utils/supabase/client-api.ts
+++ utils/supabase/client-api.ts
@@ -1,5 +1,5 @@
 
-import {createClient } from "@/lib/supabase/client";
+import {createClient } from "@/utils/supabase/client";
 
 const supabaseClient = createClient();
 export async function fetch<T>(
--- utils/supabase/server-api.ts
+++ utils/supabase/server-api.ts
@@ -1,5 +1,5 @@
 
-import {createClient} from "@/lib/supabase/server";
+import {createClient} from "@/utils/supabase/server";
 
 
 export async function fetch<T>(
